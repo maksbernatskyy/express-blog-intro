fc8fe58df7d0f41274a5dd24e28005d3eb7a0c4c
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-24 19:12:08 +0100

    add array

--- server.js
+++ server.js
@@ -2,9 +2,45 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const posts = [
+    {
+        titolo: 'Ciambellone',
+        contenuto: '',
+        immagine: '',
+        tags: ''
+    },
+    {
+        titolo: 'Cracker barbabietola',
+        contenuto: '',
+        immagine: '',
+        tags: ''
+    },
+    {
+        titolo: 'Pane fritto dolce',
+        contenuto: '',
+        immagine: '',
+        tags: ''
+    },
+    {
+        titolo: 'Pasta barbabietola',
+        contenuto: '',
+        immagine: '',
+        tags: ''
+    },
+    {
+        titolo: 'Torta paesana',
+        contenuto: '',
+        immagine: '',
+        tags: ''
+    },
+
+]
+
 app.get('/', (req, res) => {
     res.send(`Server del mio blog`)
 })
+
+app
 
 app.listen(PORT, () => {
     console.log(`Example app on port ${PORT}`)
